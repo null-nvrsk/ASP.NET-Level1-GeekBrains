6fa0cd2904bf719ed5d8c45ed137e774045493a2
NULL <nullnvrsk@gmail.com>
2021-01-24 00:44:01 +0300

    Вынос контроллера Employees

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -3,15 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
 {
-    public class HomeController : Controller
+    public class EmployeesController : Controller
     {
+        private List<Employee> _employees;
+
+        public EmployeesController()
+        {
+            _employees = TestData.employees;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_employees);
         }
     }
 }
