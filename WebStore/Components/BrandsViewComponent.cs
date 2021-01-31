0d872a96553d75b07dc3d94b3a286a48eb88c54b
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-01-31 16:58:20 +0300

    Добавлен компонент бренды и модель товара

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductData _productData;
+        public BrandsViewComponent(IProductData productData) => _productData = productData;
+
+        public IViewComponentResult Invoke() => View(GetBrands());
+
+        private IEnumerable<BrandsViewModel> GetBrands() =>
+
+            _productData.GetBrands()
+                .OrderBy(brand => brand.Order)
+                .Select(brand => new BrandsViewModel
+                { 
+                    Id = brand.Id,
+                    Name = brand.Name
+                });
+
+    }
+}
