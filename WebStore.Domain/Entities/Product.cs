8db499d36bd26ab4f1ce4e642fcd08b25812be2a
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-02-03 21:04:38 +0300

    Add entity properties for using DB

--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,21 @@ namespace WebStore.Domain.Entities
     {
         public int Order { get; set; }
 
-        public int SectionId { get; set; }
-        
-        public int? BrandId { get; set; }
 
+        public int SectionId { get; set; }
+
+        [ForeignKey(nameof(SectionId))]
+        public Section Section { get; set; }
+        
+
+        public int? BrandId { get; set; }
+        [ForeignKey(nameof(BrandId))]
+        public Brand Brand { get; set; }
+
+        
         public string ImageUrl { get; set; }
 
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
