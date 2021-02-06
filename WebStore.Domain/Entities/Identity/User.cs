9cd689c60d50c96135fa8892eeb15364fb04f85c
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-02-07 01:04:36 +0300

    Сущности User и Role

---
+++ WebStore.Domain/Entities/Identity/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Domain.Entities.Identity
+{
+    public class User : IdentityUser
+    {
+        public const string Administrator = "Admin";
+        
+        public const string PefaultAdminPassword = "Admin";
+
+        public string Description { get; set; }
+    }
+}
