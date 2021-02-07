dd12cc844bbae3b0467dd56f5ada55426b4267c8
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-02-07 22:40:10 +0300

    Вход в систему

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -21,7 +21,9 @@ namespace WebStore.Controllers
             _SignInManager = SignInManager;
         }
 
+        #region Register
 
+        [HttpGet]
         public IActionResult Register() => View(new RegisterUserViewModel());
 
         [HttpPost, ValidateAntiForgeryToken]
@@ -47,5 +49,45 @@ namespace WebStore.Controllers
 
             return View(Model);
         }
+
+        #endregion
+
+        #region Login
+
+        [HttpGet]
+        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel
+        {
+            ReturnUrl = ReturnUrl
+        });
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var login_result = await _SignInManager.PasswordSignInAsync(
+                Model.UserName,
+                Model.Password,
+                Model.RememberMe,
+#if DEBUG
+                false
+#else
+                true
+#endif
+                );
+
+            if (login_result.Succeeded)
+            {
+                return LocalRedirect(Model.ReturnUrl ?? "/");
+            }
+
+
+            ModelState.AddModelError("", "Неверное имя пользователя или пароль!");
+
+            return View(Model);
+        }
+
+        #endregion
+
     }
 }
