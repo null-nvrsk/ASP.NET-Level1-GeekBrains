b923ccd83d7702c6f693783234b8b90a35fdfcc2
NULL <nullnvrsk@gmail.com>
2021-02-07 18:04:54 +0300

    Заготовка контроллера Account

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Controllers
+{
+    //[Controller]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<Role> roleManager;
+
+        public AccountController(UserManager<User> UserManager, RoleManager<Role> RoleManager)
+        {
+            userManager = UserManager;
+            roleManager = RoleManager;
+        }
+
+    }
+}
