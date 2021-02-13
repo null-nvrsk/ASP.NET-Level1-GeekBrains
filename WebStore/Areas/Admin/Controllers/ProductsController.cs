95879e8e8b4dd24b573d81e47756df105d91f2bd
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-02-13 22:53:40 +0300

    Отображение списка товаров в админке

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -5,12 +5,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Domain.Entities.Identity;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class ProductsController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly IProductData _ProductData;
+
+        public ProductsController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Index() => View(_ProductData.GetProducts());
     }
 }
