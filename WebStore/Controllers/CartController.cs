e9113a1db151db923809dfb9b5e22f0821f2ba1a
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-02-14 16:40:49 +0300

    Оформление заказа в контроллере корзины

--- WebStore/Controllers/CartController.cs
+++ WebStore/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -12,7 +13,10 @@ namespace WebStore.Controllers
         private readonly ICartService _CartService;
 
         public CartController(ICartService CartService) => _CartService = CartService;
-        public IActionResult Index() => View(_CartService.GetViewModel());
+        public IActionResult Index() => View(new CartOrderViewModel 
+            { 
+                Cart = _CartService.GetViewModel()
+            });
 
         public IActionResult Add(int id)
         {
@@ -36,6 +40,32 @@ namespace WebStore.Controllers
         {
             _CartService.Clear();
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> CheckOut(OrderViewModel OrderModel, [FromServices] IOrderService OrderService)
+        {
+            if (!ModelState.IsValid)
+                return View(nameof(Index), new CartOrderViewModel
+                {
+                    Cart = _CartService.GetViewModel(),
+                    Order = OrderModel,
+                });
+
+            var order = await OrderService.CreateOrder(
+                User.Identity!.Name,
+                _CartService.GetViewModel(),
+                OrderModel
+                );
+
+            _CartService.Clear();
+
+            return RedirectToAction(nameof(OrderConfirmed), new { order.Id });
+        }
+
+        public IActionResult OrderConfirmed(int id)
+        {
+            ViewBag.OrderId = id;
+            return View();
         }
     }
 }
