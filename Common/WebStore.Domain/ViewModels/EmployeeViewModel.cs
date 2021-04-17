0946ce63d4e3b308cfdfdc20f2974dee85fea8a4
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-04-17 11:55:54 +0300

    Move Models & ViewModels to Domain

--- Common/WebStore.Domain/ViewModels/EmployeeViewModel.cs
+++ Common/WebStore.Domain/ViewModels/EmployeeViewModel.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
-namespace WebStore.ViewModels
+namespace WebStore.Domain.ViewModels
 {
     public class EmployeeViewModel : IValidatableObject
     {
