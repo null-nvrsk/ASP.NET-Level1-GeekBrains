b0753fc03a88f043f382be4514404e03679cee86
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-04-18 18:01:46 +0300

    View & controller for test WebAPI

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService _ValuesService;
+
+        public WebAPIController(IValuesService valuesService) { _ValuesService = valuesService; }
+        public IActionResult Index()
+        {
+            var values = _ValuesService.Get();
+            return View(values);
+        }
+    }
+}
